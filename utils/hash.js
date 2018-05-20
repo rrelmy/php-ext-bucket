a6b15146bf7ff87472d23af391403e846fdfea5f
Rémy <git@rrelmy.ch>
2018-05-20 12:47:42 +0200

    remove useless timeout / add user-agent to circumvent blocking / error handling for != 200 status codes

--- utils/hash.js
+++ utils/hash.js
@@ -21,32 +21,45 @@ function hashIt(file) {
   const name = file.match(/php-(.*).json$/)[1];
   const filePath = './nohash/' + file;
   jsonfile.readFile(filePath, (err, manifest) => {
-    if(err) return console.error({err});
-    setTimeout(function () {
-      request(manifest.architecture[key].url)
-        .on('error', function (err) {
-          console.log('error geting', name, key);
-          console.error({
-            err
-          });
-        })
-        .pipe(hasha.stream({
-          algorithm: 'sha256'
-        }))
-        .on('data', hash => {
-          manifest.architecture[key]['hash'] = hash;
-          console.log('writing hash for ' + name, key, hash);
-          jsonfile.writeFile(filePath, manifest, {
-            spaces: 4
-          }, err => {
-            if (!err) return;
-            console.error('cannot write to', filePath);
-            console.error({
-              err
+    if (err) return console.error({ err });
+    const options = {
+      url: manifest.architecture[key].url,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0'
+      }
+    };
+
+    request(options)
+      .on('error', function (err) {
+        console.log('error geting', name, key);
+        console.error({
+          err
+        });
+      })
+      .on('response', response => {
+        if (response.statusCode !== 200) {
+          console.error('error getting', name, key)
+          return;
+        }
+
+        response
+          .pipe(hasha.stream({
+            algorithm: 'sha256'
+          }))
+          .on('data', hash => {
+            manifest.architecture[key]['hash'] = hash;
+            console.log('writing hash for ' + name, key, hash);
+            jsonfile.writeFile(filePath, manifest, {
+              spaces: 4
+            }, err => {
+              if (!err) return;
+              console.error('cannot write to', filePath);
+              console.error({
+                err
+              });
             });
-          });
-        })
-    }, 5000);
+          })
+      })
 
   })
 
