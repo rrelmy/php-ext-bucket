6519379bc789ef18f3d9d86c06ea4a300b160f86
Rémy <git@rrelmy.ch>
2018-05-20 13:04:37 +0200

    do not release modules with missing hashes (most likely not available)

--- utils/release.js
+++ utils/release.js
@@ -3,6 +3,7 @@
  * You have to sepcify the php version manually
  */
 const fs = require('fs');
+const jsonfile = require('jsonfile');
 
 const phpVersion = Number(process.argv[2]);
 if (!phpVersion || phpVersion < 10 || phpVersion > 100) {
@@ -15,8 +16,19 @@ fs.readdir('./nohash', (err, list) => {
 })
 
 function releaseIt(file) {
-  const name = file.match(/php-(.*).json$/)[1];
   const filePath = './nohash/' + file;
-  
-  fs.renameSync(filePath, `../php${phpVersion}-${name.toLowerCase()}.json`);
+  jsonfile.readFile(filePath, (err, manifest) => {
+    if (err) return console.error({ err });
+
+    const name = file.match(/php-(.*).json$/)[1];
+
+    const has32bitHash = 'hash' in manifest.architecture['32bit'];
+    const has64bitHash = 'hash' in manifest.architecture['64bit'];
+    if (!has32bitHash || !has64bitHash) {
+      console.error("Missing hash for ", file, " // ", has32bitHash ? "32bit available" : "", has64bitHash ? "64bit available" : "");
+      return;
+    }
+
+    fs.renameSync(filePath, `../php${phpVersion}-${name.toLowerCase()}.json`);
+  });
 }
